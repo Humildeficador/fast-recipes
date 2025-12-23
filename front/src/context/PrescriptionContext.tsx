6af67ffd0e3377bec2e2f9d78874c3fddd110dd0
Humildeficador <jvictor.leandro@gmail.com>
2025-12-23 03:17:31 -0300

    create context

---
+++ front/src/context/PrescriptionContext.tsx
@@ -0,0 +1,43 @@
+import { createContext, useContext, useState } from 'react'
+import type { MedicationType } from '../types/types'
+
+interface PrescriptionContextData {
+  medications: MedicationType[],
+  toggleMedication: (medication: MedicationType) => void
+}
+
+const PrescriptionContext = createContext<PrescriptionContextData | null>(null)
+
+export function PrescriptionProvider({ children }: { children: React.ReactNode }) {
+  const [medications, setMedications] = useState<MedicationType[]>([])
+
+  function toggleMedication(medication: MedicationType) {
+    setMedications((prev) => {
+      const exists = prev.some(med => med.id === medication.id)
+
+      if (exists) {
+        return prev.filter(med => med.id !== medication.id)
+      }
+
+      return [...prev, medication]
+    })
+  }
+
+  return (
+    <PrescriptionContext.Provider value={{ medications, toggleMedication }}>
+      {children}
+    </PrescriptionContext.Provider>
+  )
+}
+
+
+// eslint-disable-next-line react-refresh/only-export-components
+export function usePrescription() {
+  const context = useContext(PrescriptionContext)
+
+  if (!context) {
+    throw new Error('usePrescription must be used within a PrescriptionProvider')
+  }
+
+  return context
+}
--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,15 +1,18 @@
 import './index.css'
 import { App } from './App.tsx'
 import { StrictMode } from 'react'
+import { Container } from '@chakra-ui/react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from './components/ui/provider.tsx'
-import { Container } from '@chakra-ui/react'
+import { PrescriptionProvider } from './context/PrescriptionContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
       <Container pt={4}>
-        <App />
+        <PrescriptionProvider >
+          <App />
+        </PrescriptionProvider>
       </Container>
     </Provider>
   </StrictMode>,
